4c8a74d662dd5047ee84bf9329cbcb1e3af7179a
oasierra5 <oasierra5@misena.edu.co>
2022-08-30 15:53:03 -0500

    Actualizamos agregar película, listar película, alquilar película

--- videoClub/a_VideoClub.py
+++ videoClub/a_VideoClub.py
@@ -21,6 +21,41 @@ class VideoClub:
         return False
     
     def agregar_socio(self, socio):
-        
         self.socios.append(socio)
-        
+
+    def eliminar_socio(self, dni):
+        for i in range(len(self.socios)):
+            if self.socios[i].dni == dni:
+                del self.socios[i]
+                break
+    
+    def nueva_pelicula(self):
+        codigo = input('Codigo: ')
+        titulo = input('Titulo: ')
+        genero = input('Genero: ')
+        return Pelicula(codigo, titulo, genero)
+
+    def contiene_pelicula(self, codigo):
+        for peli in self.pelicula:
+            if peli.codigo == codigo:
+                return True
+        return False
+
+    def agregar_pelicula(self, pelicula):
+        self.pelicula.append(pelicula)
+
+    def listar_peliculas(self):
+        for pel in self.pelicula:
+            print("Código: ",pel.codigo)
+            print("Titulo: ",pel.titulo)
+            print("Estado: ",pel.alquilada)
+            print("##################################")
+
+    def alquilar_pelicula(self, codigo, dni):
+        for pel in self.pelicula:
+            if pel.codigo == codigo:
+                if pel.alquilada == None:
+                    pel.alquilada = dni
+                    return True
+                else:
+                    return False
